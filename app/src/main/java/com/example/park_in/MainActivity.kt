d9adfdf02804bb68197fa8c2bfb4810a4f74348b
norbertovargas <norbertovargasislaverde@gmail.com>
2021-11-03 00:21:20 +0100

    prueba4

--- app/src/main/java/com/example/park_in/MainActivity.kt
+++ app/src/main/java/com/example/park_in/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //asfasdfasdfasdf
         //sdf
-        //45646
-        //4544
+        //456
     }
 }
